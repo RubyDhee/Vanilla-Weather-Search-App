b4126aba3e7b713fb99f3e1a92d147e76403b677
RubyDhee <90288362+RubyDhee@users.noreply.github.com>
2021-11-08 02:39:19 +0100

    Update app.js
    added functions for temperature and date

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0 ${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}: ${minutes}`;
+}
 
+function showTemperature(response) {
+  console.log(response.data);
+  let cityTemp = Math.round(response.data.main.temp);
+  let listTemperature = document.querySelector("#temperature");
+  listTemperature.innerHTML = cityTemp;
+  let cityName = document.querySelector(".city");
+  cityName.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let city = "Marseille";
+let units = "metric";
+let apiKey = "5354b60afda2b7800186c06153932396";
+let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(showTemperature);
