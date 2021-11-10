46ca680dfe9cf106223097850b3c3d70fb61fc57
RubyDhee <90288362+RubyDhee@users.noreply.github.com>
2021-11-10 03:11:21 +0100

    added conversion functions + adjustments

--- src/app.js
+++ src/app.js
@@ -23,20 +23,21 @@ function formatDate(timestamp) {
 
 function showTemperature(response) {
   console.log(response.data);
-  let cityTemp = Math.round(response.data.main.temp);
-  let listTemperature = document.querySelector("#temperature");
-  listTemperature.innerHTML = cityTemp;
+  let temperatureElement = document.querySelector("#temperature");
   let cityName = document.querySelector(".city");
-  cityName.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityName.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -58,7 +59,32 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("Skopje");
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector(".searchCity");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#convert-fahrenheit");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#convert-celsius");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+search("Skopje");
